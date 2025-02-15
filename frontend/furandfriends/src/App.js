5222d3b1987427d5faccb33e19f4482d37ebbde8
Shawna <shawnakatrina.halladeen@gmail.com>
2025-02-15 09:25:36 -0500

    working on signup page

--- frontend/furandfriends/src/App.js
+++ frontend/furandfriends/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Signup from './pages/Signup';
+import Signup from '../src/pages/Signup/Signup';
 import ElderlyWelcome from '../src/pages/WelcomeElder';
 import VolunteerWelcome from '../src/pages/WelcomeVolunteer';
-import Navbar from '../src/components/Navbar/Navbar'
 
 const App = () => {
   return (
     <Router>
-      <Navbar/>
       <Routes>
-        <Route path="/" element={<ElderlyWelcome />} />  {/* ✅ Elderly page is now the default */}
+        <Route path="/" element={<ElderlyWelcome />} /> 
         <Route path="/signup" element={<Signup />} />
         <Route path="/welcome-elderly" element={<ElderlyWelcome />} />
         <Route path="/welcome-volunteer" element={<VolunteerWelcome />} />
