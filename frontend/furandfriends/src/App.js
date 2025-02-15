47a456e19c404caa6e982e6a70bcb8afdb3f63db
Lynaye <Lynaye@hotmail.ca>
2025-02-15 08:44:47 -0500

    navbar

--- frontend/furandfriends/src/App.js
+++ frontend/furandfriends/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './pages/Signup';
 import ElderlyWelcome from '../src/pages/WelcomeElder';
 import VolunteerWelcome from '../src/pages/WelcomeVolunteer';
+import Navbar from '../src/components/Navbar/Navbar'
 
 const App = () => {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<ElderlyWelcome />} />  {/* ✅ Elderly page is now the default */}
         <Route path="/signup" element={<Signup />} />
