5afc30b32f7f63b54666e13aa0e8f0baed088a81
Krishna <krishnamehta0310@gmail.com>
2025-02-15 15:54:57 -0500

    fixed all dimensions and navbar

--- frontend/furandfriends/src/components/Navbar/Navbar.js
+++ frontend/furandfriends/src/components/Navbar/Navbar.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import '../Navbar/Navbar.css';
 import logo from '../Images/Logo.png';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
+      {/* Left Section (Logo) */}
       <div className="navbar-left">
         <Link to="/" className="logo">
           <img src={logo} alt="Fur and Friends Logo" className="logo-img" />
         </Link>
         <span className="logo-text">Fur & Friends</span>
       </div>
-      <div className="navbar-center">
-        <ul className="nav-links">
-          <li>
-            <Link to="/Connect" className="button-link">Connect</Link>
-          </li>
-          <li>
-            <Link to="/Volunteer-form" className="button-link">Volunteer</Link>
-          </li>
-          <li>
-            <Link to="/Contact" className="button-link">Contact</Link>
-          </li>
-        </ul>
+
+      {/* Hamburger Icon (Mobile) */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
-      <div className="navbar-right">
-        <Link to="/signup" className="button-link">
-          Sign Up / Login
-        </Link>
-      </div>
+
+      {/* Center Section (Nav Links) */}
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+        <li><Link to="/Connect" className="button-link" onClick={() => setMenuOpen(false)}>Connect</Link></li>
+        <li><Link to="/Volunteer-form" className="button-link" onClick={() => setMenuOpen(false)}>Volunteer</Link></li>
+        <li><Link to="/Contact" className="button-link" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+        {/* This Sign Up / Login button should ONLY be visible in the mobile menu */}
+        <li className="mobile-signup"><Link to="/signup" className="button-link" onClick={() => setMenuOpen(false)}>Sign Up / Login</Link></li>
+      </ul>
+
     </nav>
   );
 };
